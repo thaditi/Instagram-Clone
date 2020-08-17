f362196c2bdb2f7fe3c2ad1f25468feccf972e6f
Aditi Thapliyal <47235301+thaditi@users.noreply.github.com>
2020-08-17 18:47:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBDFIUUOis__66y1_dz4CoC41qSPvKahAQ",
-  authDomain: "instagram-clone-fd4b2.firebaseapp.com",
-  databaseURL: "https://instagram-clone-fd4b2.firebaseio.com",
-  projectId: "instagram-clone-fd4b2",
-  storageBucket: "instagram-clone-fd4b2.appspot.com",
-  messagingSenderId: "644483085537",
-  appId: "1:644483085537:web:fc413b5cd4b8047bbf73a0",
-  measurementId: "G-X6REYP7RER",
+ //firebase API KEY
 });
 
 const db = firebaseApp.firestore();
